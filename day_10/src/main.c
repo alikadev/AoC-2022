37fb07b7a4c21477b1cb5b526764f8eb31aef6a3
alikadev <def@alikadev.com>
2022-12-10 20:17:59 +0100

    Rename single digit day to double digit (day_N -> day_0N)

---
+++ day_10/src/main.c
@@ -0,0 +1,24 @@
+#define AOC_IN_FILE "data.in"
+#define AOC_OUT_FILE "data.out"
+#define AOC_OUT_FORMAT "%d\n"
+
+static int result = 0;
+
+#include "aocapp.h"
+
+/* ===== AoC INCLUDES ===== */
+
+/**
+ * The function that start to process the data
+ * @param data A c_str that contains the input
+ */
+void start(char *data)
+{
+	printf("FILE CONTENT: \n%s\n", data);
+}
+
+
+
+/* ===== MAIN FUNCTION ===== */
+
+#include "aocmain.h"
